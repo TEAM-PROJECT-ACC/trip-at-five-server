42cbbb093815ae2ab0df0c41e46961ff16c3e63b
Seong-Jun1525 <sjsj123455@naver.com>
2025-06-16 15:28:09 +0900

    Feat: 예약취소요청 페이지 페이징처리 및 검색 기능 구현

--- src/main/java/com/kh/clock/admin/controller/AdminReservationController.java
+++ src/main/java/com/kh/clock/admin/controller/AdminReservationController.java
@@ -6,10 +6,13 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.kh.clock.accommodation.repository.dto.AccomAdminListDTO;
+import com.kh.clock.accommodation.repository.dto.AccomAdminSearchDTO;
 import com.kh.clock.admin.repository.dto.AdminReservationCancelListDTO;
 import com.kh.clock.admin.repository.dto.AdminReservationDetailDTO;
 import com.kh.clock.admin.repository.dto.AdminReservationListDTO;
@@ -28,6 +31,13 @@ public class AdminReservationController {
     this.adminReservationService = adminReservationService;
   }
   
+  /**
+   * 예약 전체 조회
+   * @param keyword
+   * @param currentPage
+   * @param numOfRows
+   * @return
+   */
   @GetMapping
   public ResponseEntity<Map<String, Object>> selectReservationList(
       @RequestParam(defaultValue = "") String keyword,
@@ -45,20 +55,34 @@ public class AdminReservationController {
       Map<String, Object> response = new HashMap<>();
       response.put("dataList", reservations);
       response.put("totalCount", pageInfo.getTotalCount());
+      
+      System.out.println(response);
 
       return ResponseEntity.ok(response);
   }
 
+  /**
+   * 예약 상세 조회
+   * @param resCd
+   * @return
+   */
   @GetMapping("/{resCd}")
   public ResponseEntity<Object> reservationDetail(@PathVariable String resCd) {
     AdminReservationDetailDTO resVO = adminReservationService.findReservationByResCd(resCd);
     
-    System.out.println(resVO);
+    System.out.println("resVO: " + resVO);
     
     if(resVO != null) return ResponseEntity.status(HttpStatus.OK).body(resVO);
     else return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("예약 상세 조회에 실패했습니다.");
   }
   
+  /**
+   * 예약 취소 요청 관리 목록
+   * @param keyword
+   * @param currentPage
+   * @param numOfRows
+   * @return
+   */
   @GetMapping("/cancel/list")
   public ResponseEntity<Object> reservationCancelList(
       @RequestParam(defaultValue = "") String keyword,
